b77732764830e8e1332346a9013078293436f116
ADITHYA-37 <adithyasurisetti9@gmail.com>
2026-07-24 21:18:02 +0530

    Time: 0 ms (100%), Space: 9 MB (70.11%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.c
@@ -0,0 +1,16 @@
+int searchInsert(int* a, int n, int p) {
+    int l=0,h=n-1,m;
+    while(l<=h){
+        m=(l+h)/2;
+        if (a[m]==p){
+            return m;
+        }
+        else if(a[m]>p){
+            h=m-1;
+        }
+        else{
+            l=m+1;
+        }
+    }
+    return h+1;
+}

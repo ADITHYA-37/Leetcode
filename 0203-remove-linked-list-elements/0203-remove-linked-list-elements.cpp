21c4470b1fd7669e9801969df730da58a4391435
Adithya <adithyasurisetti9@gmail.com>
2026-09-07 22:16:48 +0530

    Time: 0 ms (100%), Space: 20.1 MB (44.92%) - LeetHub

--- 0203-remove-linked-list-elements/0203-remove-linked-list-elements.cpp
+++ 0203-remove-linked-list-elements/0203-remove-linked-list-elements.cpp
@@ -11,21 +11,21 @@
 class Solution {
 public:
     ListNode* removeElements(ListNode* head, int k) {
-        if (head == NULL){
+        if (head == NULL){//empty list
             return head;
         }
-        while (head != NULL && head->val == k) {//order is imp 
+        while (head != NULL && head->val == k) {//order is imp //loop is for removing repeated vals in beginning
             head = head->next;
         }
-        struct ListNode* p = head;
+        struct ListNode* p = head;//prev node
         if (p == NULL){
             return head;
         }
-        struct ListNode* c = head->next;
+        struct ListNode* c = head->next;//current node
         while (c!=NULL){
             if (c->val == k){
                 p->next = c->next;
-                c = c->next;
+                c = c->next; // as the current node is being removed but prev still exists
                 continue;
             }
             p = p->next;

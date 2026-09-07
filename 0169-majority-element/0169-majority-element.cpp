bebfb729d7ba7fe73a9a5ba41e2899621c93d2ba
Adithya <adithyasurisetti9@gmail.com>
2026-09-07 22:56:11 +0530

    Time: 0 ms (100%), Space: 28.4 MB (14.68%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int majorityElement(vector<int>& a) {
+    int majorityElement(vector<int>& a) { //this soln is typical , a more personalised soln is done in C
         unordered_map<int,int> mp;
         for (int i : a){
             mp[i]++;
